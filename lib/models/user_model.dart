40ef85d08315a2c950e00957eee4926a3623b38c
Mayankjn25 <mayank.jain.25.1999@gmail.com>
2021-11-26 04:16:43 +0530

    added user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String token;
+
+  const User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.token,
+  });
+
+  factory User.fromDoc(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return User(
+      id: doc.id,
+      name: data['name'] ?? '',
+      email: data['email'] ?? '',
+      token: data['token'] ?? '',
+    );
+  }
+}
